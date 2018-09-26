7ecb6a08f93a50e16ecaa88f75af3f50a981234f
jiangh2 <Ydong6688@>
2018-09-26 17:00:56 -0400

    fff

--- src/m3r_accumulating_sequences.py
+++ src/m3r_accumulating_sequences.py
@@ -5,7 +5,7 @@ one item at a time, using the ACCUMULATOR pattern.
         sequences, namely by MUTATING their elements.
 
 Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
+         their colleagues and Hao Jiang.
 """  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 # ----------------------------------------------------------------------
